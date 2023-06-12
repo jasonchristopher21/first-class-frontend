2eefdd5d43e4ba278890664c9bb05451ac7483e9
florentianayuwono <florentianayuwono@gmail.com>
2023-06-12 18:21:02 +0800

    Add App structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Sidebar } from '../components/Sidebar'
+import { Main } from '../components/Main'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Sidebar />
+        <div>
+            <Main />
+        </div>
+    </div>
   )
 }
