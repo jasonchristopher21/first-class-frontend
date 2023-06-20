edec0e17dc7049867021e146a470062c2d038e8c
Jason Christopher <jason.christopher@u.nus.edu>
2023-06-21 01:48:43 +0800

    Add Colors and Text Styles to Tailwind

---
+++ src/style.js
@@ -0,0 +1,22 @@
+const styles = {
+
+    "heading1": "font-sans font-bold text-[32px]",
+    "heading2": "font-sans font-bold text-[24px]",
+    "heading3": "font-sans font-bold text-[20px]",
+    "heading4": "font-sans font-bold text-[18px]",
+    "heading5": "font-sans font-bold text-[14px]",
+
+    "paragraph1": "font-sans text-[18px]",
+    "paragraph2": "font-sans font-medium text-[18px]",
+    "paragraph3": "font-sans text-[16px]",
+    "paragraph4": "font-sans font-medium text-[16px]",
+    "paragraph5": "font-sans text-[14px]",
+    "paragraph6": "font-sans font-medium text-[14px]",
+    "paragraph7": "font-sans text-[12px]",
+    "paragraph8": "font-sans font-medium text-[12px]",
+    "paragraph9": "font-sans text-[10px]",
+    "paragraph10": "font-sans font-medium text-[10px]",
+
+}
+
+export default styles
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "black": "#1F201E",
+      "dark-grey": "#888888",
+      "light-grey": "#EFEFEF",
+      "white": "#FFFFFF",
+      "orange": "#FFAE00",
+      "yellow": "#FFE900",
+      "yellow-accent": "#FFFBCC",
+    },
+    fontFamily: {
+      'sans': ['Montserrat', 'sans-serif']
+    }
   },
   plugins: [],
-}
-
+};
