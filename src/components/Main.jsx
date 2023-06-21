b1dc0c923c7f6ac391972fb9e40114317eed5833
florentianayuwono <florentianayuwono@gmail.com>
2023-06-21 15:40:40 +0800

    Add banner

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "../index.css";
 
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div>
+      <div className="banner fixed top-0 left-0 w-full h-1/4 z-0 bg-yellow">
+        <div className="px-10 pt-14 text-left">
+          <p className="font-normal">Welcome onboard TR252,</p>
+          <p className="font-bold text-2xl">Florentiana Yuwono</p>
+        </div>
+      </div>
+    </div>
+  );
+};
