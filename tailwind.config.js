bd3285e76840e07088cb21f0a12508a66c627b02
florentianayuwono <florentianayuwono@gmail.com>
2023-06-21 11:21:26 +0800

    Add screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ export default {
     },
     fontFamily: {
       'sans': ['Montserrat', 'sans-serif']
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+      tall: {'raw': '(min-height: 768px) and (min-width: 768px)'}
     }
   },
   plugins: [],
