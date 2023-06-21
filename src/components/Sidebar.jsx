9ab6ce4e01d48848a3944a476a0961ab46289f41
florentianayuwono <florentianayuwono@gmail.com>
2023-06-21 14:30:31 +0800

    Style sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,54 @@
-import React from 'react'
-import { icon_cart, icon_home, icon_menubar, icon_order } from '../assets'
+import React, { useRef, useEffect, useState } from "react";
+import Draggable from "react-draggable";
+import { icon_cart, icon_home, icon_menubar, icon_order } from "../assets";
+import "../index.css";
 
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavBar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const Dropdown = () => {
+    return (
+      <div>
+        <ul>
+          <li>
+            <div className="nav-content">
+              <img src={icon_home} />
+            </div>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <div className="nav-content">
+              <img src={icon_cart} />
+            </div>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <div className="nav-content">
+              <img src={icon_order} />
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
   return (
-    <div>
-        <ul>
-            <li>
-                <button>
-                    <img src={icon_menubar} />
-                </button>
-            </li>
-        </ul>
+    <nav>
+      <div className="rounded-full bg-white p-4">
+        <div className="nav-content" onClick={toggleNavBar}>
+          <img src={icon_menubar} />
+        </div>
 
-        <ul>
-            <li>
-                <button>
-                    <img src={icon_home} />
-                </button>
-            </li>
-        </ul>
-
-        <ul>
-            <li>
-                <button>
-                    <img src={icon_cart} />
-                </button>
-            </li>
-        </ul>
-
-        <ul>
-            <li>
-                <button>
-                    <img src={icon_order} />
-                </button>
-            </li>
-        </ul>
-    </div>
-  )
-}
+        {isOpen && <Dropdown />}
+      </div>
+    </nav>
+  );
+};
