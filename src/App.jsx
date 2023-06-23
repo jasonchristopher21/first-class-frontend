72b6ec861b6a66ef5374db00622a448b8feb54ef
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-24 03:29:41 +0800

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Home from "./pages/Home";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Login } from "./pages/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <Login/>
     </div>
   );
 }
