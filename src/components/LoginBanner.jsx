91cef0e3adbbd66f8e9dccb52668128c9de26f85
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-06-25 18:19:05 +0800

    Add error messages and staff login handler

--- src/components/LoginBanner.jsx
+++ src/components/LoginBanner.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../style';
-
 import { store } from '../redux/store';
 import { getUser } from '../redux/features/auth/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { getStaff } from '../redux/features/auth/staffAuthSlice';
 
 export const LoginBanner = () => {
   const [passengerMode, setPassengerMode] = useState(true);
@@ -11,7 +12,14 @@ export const LoginBanner = () => {
   const [seatNumber, setSeatNumber] = useState('');
   const [staffId, setStaffId] = useState('');
 
+  const [showSurnameError, setShowSurnameError] = useState(false);
+  const [showFlightNumberError, setShowFlightNumberError] = useState(false);
+  const [showSeatNumberError, setShowSeatNumberError] = useState(false);
+  const [showStaffIdError, setShowStaffIdError] = useState(false);
+
   const arrow = <span>&#x1F806;</span>;
+
+  const navigate = useNavigate();
 
   const passengerModeInputInstructions = [
     'Enter your surname, flight number',
@@ -28,39 +36,24 @@ export const LoginBanner = () => {
       setSurname('');
       setFlightNumber('');
       setSeatNumber('');
+      setShowStaffIdError(false);
+      setShowFlightNumberError(false);
     } else {
       setPassengerMode(true);
       setStaffId('');
-      ('');
       setFlightNumber('');
+      setShowSurnameError(false);
+      setShowFlightNumberError(false);
+      setShowSeatNumberError(false);
     }
   };
 
-  const handlePassengerButtonClick = () => {
-    console.log(surname);
-    console.log(flightNumber);
-    console.log(seatNumber);
-    // fetch('/auth/user/login', {
-    //   method: 'POST',
-    //   headers: JSON.stringify({
-    //     Accept: 'application.json',
-    //     'Content-Type': 'application/json',
-    //   }),
-    //   body: {
-    //     surname: surname,
-    //     flightNumber: flightNumber,
-    //     seatNumber: seatNumber,
-    //   },
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data));
-  };
-
-  const LoginButton = (props) => {
+  const LoginButton = ({ onClick }) => {
     return (
       <div className="w-[100%] text-center pb-4 pt-4">
         <button
-          className={`${styles.heading4} rounded-full bg-yellow w-[100%] focus:outline-0 focus:bg-yellow-accent text-black`} onClick={props.func}>
+          className={`${styles.heading4} rounded-full bg-yellow w-[100%] focus:outline-0 focus:bg-yellow-accent text-black`}
+          onClick={onClick}>
           Log in
         </button>
       </div>
@@ -79,6 +72,14 @@ export const LoginBanner = () => {
     );
   };
 
+  const ErrorMessage = ({ message }) => {
+    return (
+      <div className="pt-2">
+        <p className={`${styles.paragraph6} text-red`}>{message}</p>
+      </div>
+    );
+  };
+
   const LoginAsText = ({ role }) => {
     return (
       <div className="w-[100%] text-center">
@@ -92,8 +93,61 @@ export const LoginBanner = () => {
   };
 
   const handlePassengerLogin = () => {
-    store.dispatch(getUser({ surname: "Yuwono", flightNumber: 'TR2542', seatNumber: '16K' }));
-  }
+    if (surname === '') {
+      setShowSurnameError(true);
+      return;
+    }
+
+    if (flightNumber === '') {
+      setShowFlightNumberError(true);
+      return;
+    }
+
+    if (seatNumber === '') {
+      setShowSeatNumberError(true);
+      return;
+    }
+
+    store.dispatch(
+      getUser({
+        surname: surname,
+        flightNumber: flightNumber,
+        seatNumber: seatNumber,
+      })
+    );
+    navigate('home');
+  };
+
+  const handleStaffLogin = () => {
+    if (staffId === '') {
+      setShowStaffIdError(true);
+      return;
+    }
+
+    if (flightNumber === '') {
+      setShowFlightNumberError(true);
+      return;
+    }
+
+    store.dispatch(getStaff({ staffId: staffId, flightNumber: flightNumber }));
+    navigate('home');
+  };
+
+  useEffect(() => {
+    setShowSurnameError(false)
+  }, [surname])
+
+  useEffect(() => {
+    setShowFlightNumberError(false);
+  }, [flightNumber])
+
+  useEffect(() => {
+    setShowSeatNumberError(false);
+  }, [seatNumber]);
+
+  useEffect(() => {
+    setShowStaffIdError(false);
+  }, [staffId])
 
   return (
     <div className="h-[75%] bg-white rounded-t-3xl shadow-[0px 0px 20px 0px rgba(0, 0, 0, 0.25)] flex content-center items-center flex-col">
@@ -111,7 +165,11 @@ export const LoginBanner = () => {
                 className={` ${styles.paragraph6} w-[100%] bg-[#00000010] rounded-xl text-black focus:outline-0 p-3`}
                 onChange={(e) => setSurname(e.currentTarget.value)}
                 value={surname}></input>
+              {showSurnameError ? (
+                <ErrorMessage message="Please fill in your surname." />
+              ) : null}
             </div>
+
             <div className="w-[100%] text-left pb-4">
               <p className={`${styles.paragraph6} text-black pb-1`}>
                 Flight Number
@@ -120,6 +178,7 @@ export const LoginBanner = () => {
                 className={` ${styles.paragraph6} w-[100%] bg-[#00000010] rounded-xl text-black focus:outline-0 p-3`}
                 onChange={(e) => setFlightNumber(e.currentTarget.value)}
                 value={flightNumber}></input>
+              {showFlightNumberError ? <ErrorMessage message="Please fill in your flight number." /> : null}
             </div>
             <div className="w-[100%] text-left pb-4">
               <p className={`${styles.paragraph6} text-black pb-1`}>
@@ -129,8 +188,9 @@ export const LoginBanner = () => {
                 className={` ${styles.paragraph6} w-[100%] bg-[#00000010] rounded-xl text-black focus:outline-0 p-3`}
                 onChange={(e) => setSeatNumber(e.currentTarget.value)}
                 value={seatNumber}></input>
+              { showSeatNumberError ? <ErrorMessage message="Please fill in your seat number." /> : null }
             </div>
-            <LoginButton onClick={handlePassengerButtonClick} />
+            <LoginButton onClick={handlePassengerLogin} />
             <LoginAsText role="STAFF MEMBER" />
           </>
         ) : (
@@ -142,6 +202,7 @@ export const LoginBanner = () => {
                 className={` ${styles.paragraph6} w-[100%] bg-[#00000010] rounded-xl text-black focus:outline-0 p-3`}
                 onChange={(e) => setStaffId(e.currentTarget.value)}
                 value={staffId}></input>
+              {showStaffIdError ? <ErrorMessage message="Please fill in your staff ID." /> : null }
             </div>
             <div className="w-[100%] text-left pb-4">
               <p className={`${styles.paragraph6} text-black pb-1`}>
@@ -150,11 +211,10 @@ export const LoginBanner = () => {
               <input
                 className={` ${styles.paragraph6} w-[100%] bg-[#00000010] rounded-xl text-black focus:outline-0 p-3`}
                 onChange={(e) => setFlightNumber(e.currentTarget.value)}
-                value={seatNumber}></input>
+                value={flightNumber}></input>
+              {showFlightNumberError ? <ErrorMessage message="Please fill in your flight number." /> : null}
             </div>
-            <LoginButton
-              onClick={passengerMode ? handlePassengerButtonClick : null}
-            />
+            <LoginButton onClick={handleStaffLogin} />
             <LoginAsText role="PASSENGER" />
           </>
         )}
